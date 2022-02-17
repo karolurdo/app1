84c3915ffbe93709aae9911ef78ee9ed20ce03a9
Urdo <karol.ugarenko@gmail.com>
2022-02-17 19:06:09 +0100

    for ( of )

--- app1.js
+++ app1.js
@@ -1,7 +1,21 @@
 const names = [];
+const div = document.querySelector('div');
 
 const addName = (e) => {
-    e.preventDefault();  // stop refreshing web
+    e.preventDefault(); // stop refreshing web
+const input = document.querySelector('input');
+const newName = input.value;
+//console.log(name); //check input.value
+if (input.value.length) {
+    for (name of names){
+        if (name.toUpperCase() === newName.toUpperCase()) {
+        return; // end when name arledy in []
+    }
 }
+names.push(newName);
+//console.log(names);
+div.textContent += newName + " ,";
+input.value = "";
+}}
 
 document.querySelector("button").addEventListener('click', addName);
